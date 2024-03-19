4c5a1840d0531b76c605dd24679e8f25252fde10
lesleyferreira <lesley.ferreira.tw@naturapay.net>
2024-03-19 10:25:24 -0300

    chore: add broadcast

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -1,7 +1,11 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.HandlerThread
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +20,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var incrementServiceIntent: Intent
     private var counter = 0
+    private val incrementBroadcastReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.getIntExtra("VALUE", -1)?.also { value ->
+                counter = value
+                Toast.makeText(this@MainActivity, "You clicked $counter times", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +44,17 @@ class MainActivity : AppCompatActivity() {
                 })
             }
         }
-        InterEntityCommunication.valueLiveData.observe(this){ value ->
-            counter = value
-            Toast.makeText(this, "You clicked $counter times", Toast.LENGTH_SHORT).show()
-        }
     }
 
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(incrementBroadcastReceiver, IntentFilter("INCREMENT_VALUE_ACTION"))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(incrementBroadcastReceiver)
+    }
     override fun onDestroy() {
         super.onDestroy()
         startService(incrementServiceIntent)
